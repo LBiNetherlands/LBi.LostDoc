4c2a235432945b856353d11161a7289c3e6b4807
Patrik Husfloen <patrik.husfloen@lbi.com>
2013-02-15 16:56:02 +0100

    Added initial Less support

---
+++ LBi.LostDoc.Templating.Transforms.Less/LessTransform.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+using dotless.Core.configuration;
+
+namespace LBi.LostDoc.Templating.Transforms.Less
+{
+    [ExportTransform("less")]
+    public class LessTransform : IResourceTransform
+    {
+        public Stream Transform(Stream input)
+        {
+            using (StreamReader reader = new StreamReader(input, true))
+            {
+                string lessDoc = dotless.Core.Less.Parse(reader.ReadToEnd(),
+                                                         new DotlessConfiguration
+                                                             {
+                                                                 CacheEnabled = false,
+                                                                 Debug = false,
+                                                             });
+                return new MemoryStream(Encoding.UTF8.GetBytes(lessDoc)) {Position = 0};
+            }
+        }
+    }
+}
