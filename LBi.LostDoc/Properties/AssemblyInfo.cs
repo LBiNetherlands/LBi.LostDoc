81c3981e7190e7cce7d904e5e3b7861cbfd50a3c
Patrik Husfloen <patrik.husfloen@lbi.com>
2013-02-15 16:37:50 +0100

    Fixed visibility issued casued by NS changes

--- LBi.LostDoc/Properties/AssemblyInfo.cs
+++ LBi.LostDoc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LBi.LostDoc.Core")]
+[assembly: AssemblyTitle("LBi.LostDoc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LBi.LostDoc.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyProduct("LBi.LostDoc")]
+[assembly: AssemblyCopyright("Copyright © LBi Netherlands 2011, 2012, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("LBi.LostDoc.Core.Test")]
+[assembly: InternalsVisibleTo("LBi.LostDoc.Test")]
