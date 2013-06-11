6cb723e063840bb1c22634aa52f354105fcc23aa
Patrik Husfloen <patrik.husfloen@lbi.com>
2013-06-11 22:04:00 +0200

    Refactored AdminFilter

--- LBi.LostDoc.Repository.Web.Host/Areas/Administration/AdminFilter.cs
+++ LBi.LostDoc.Repository.Web.Host/Areas/Administration/AdminFilter.cs
@@ -21,99 +21,114 @@ namespace LBi.LostDoc.Repository.Web.Areas.Administration
         public void OnActionExecuted(ActionExecutedContext filterContext)
         {
             ViewResult viewResult = filterContext.Result as ViewResult;
-            if (viewResult != null && viewResult.Model != null)
+            if (viewResult == null || viewResult.Model == null)
+                return;
+
+            // this was populated by the AddInControllerFactory
+            IControllerMetadata metadata =
+                (IControllerMetadata)
+                filterContext.RequestContext.HttpContext.Items[AddInControllerFactory.MetadataKey];
+
+            if (metadata == null)
+                return;
+
+            IAdminModel model = viewResult.Model as IAdminModel;
+            if (model == null)
+                throw new InvalidOperationException(string.Format("Model ({0}) does not implement IAdminModel",
+                                                                  viewResult.GetType().Name));
+
+            model.Notifications = this.Notifications.Get(filterContext.HttpContext.User).ToArray();
+
+
+            var catalog = App.Instance.Container.Catalog;
+
+            Navigation active;
+            var navigations = BuildMenu(filterContext, catalog, out active);
+            model.Navigation = navigations;
+
+            model.PageTitle = "LostDoc Administration - " + metadata.Text;
+
+            if (!string.IsNullOrWhiteSpace(active.Text))
+                model.PageTitle += string.Format(" - {0}", active.Text);
+        }
+
+        private static Navigation[] BuildMenu(ActionExecutedContext filterContext,
+                                              ComposablePartCatalog catalog,
+                                              out Navigation active)
+        {
+            active = null;
+            List<Navigation> menu = new List<Navigation>();
+            foreach (var exportInfo in catalog.FindExports<IControllerMetadata>(ContractNames.AdminController))
             {
-                IAdminModel model = viewResult.Model as IAdminModel;
-                if (model == null)
-                    throw new InvalidOperationException(string.Format("Model ({0}) does not implement IAdminModel", 
-                                                                      viewResult.GetType().Name));
+                ReflectedControllerDescriptor descriptor = new ReflectedControllerDescriptor(exportInfo.Value);
 
-                model.Notifications = this.Notifications.Get(filterContext.HttpContext.User).ToArray();
+                var controllerAttr =
+                    descriptor.GetCustomAttributes(typeof(AdminControllerAttribute), true).FirstOrDefault()
+                    as AdminControllerAttribute;
 
-                // this was populated by the AddInControllerFactory
-                IControllerMetadata metadata =
-                    (IControllerMetadata)
-                    filterContext.RequestContext.HttpContext.Items[AddInControllerFactory.MetadataKey];
+                if (controllerAttr == null)
+                    continue;
 
-                string actionText = string.Empty;
-                var catalog = App.Instance.Container.Catalog;
-
-                List<Navigation> menu = new List<Navigation>();
-                foreach (var exportInfo in catalog.FindExports<IControllerMetadata>(ContractNames.AdminController)) 
+                UrlHelper urlHelper = new UrlHelper(filterContext.RequestContext);
+                Uri defaultTargetUrl = null;
+                List<Navigation> children = new List<Navigation>();
+                foreach (var actionDescriptor in descriptor.GetCanonicalActions())
                 {
-                    ReflectedControllerDescriptor descriptor = new ReflectedControllerDescriptor(exportInfo.Value);
-
-                    var controllerAttr =
-                        descriptor.GetCustomAttributes(typeof (AdminControllerAttribute), true).FirstOrDefault()
-                        as AdminControllerAttribute;
-
-                    if (controllerAttr == null)
+                    var actionAttr =
+                        actionDescriptor.GetCustomAttributes(typeof(AdminActionAttribute), true).FirstOrDefault() as
+                        AdminActionAttribute;
+                    if (actionAttr == null)
                         continue;
 
-                    UrlHelper urlHelper = new UrlHelper(filterContext.RequestContext);
-                    Uri defaultTargetUrl = null;
-                    List<Navigation> children = new List<Navigation>();
-                    foreach (var actionDescriptor in descriptor.GetCanonicalActions())
-                    {
-                        var actionAttr =
-                            actionDescriptor.GetCustomAttributes(typeof(AdminActionAttribute), true).FirstOrDefault() as
-                            AdminActionAttribute;
-                        if (actionAttr == null)
-                            continue;
-
-                        // TODO replace anon class with concrete type?
-                        string targetUrl = urlHelper.Action(actionAttr.Name,
-                                                            controllerAttr.Name,
-                                                            new
+                    // TODO replace anon class with concrete type?
+                    string targetUrl = urlHelper.Action(actionAttr.Name,
+                                                        controllerAttr.Name,
+                                                        new
                                                             {
                                                                 packageId = exportInfo.Metadata.PackageId,
                                                                 packageVersion = exportInfo.Metadata.PackageVersion
                                                             });
 
-                        Uri target = new Uri(targetUrl, UriKind.RelativeOrAbsolute);
+                    Uri target = new Uri(targetUrl, UriKind.RelativeOrAbsolute);
 
-                        if (defaultTargetUrl == null || actionAttr.IsDefault)
-                            defaultTargetUrl = target;
+                    if (defaultTargetUrl == null || actionAttr.IsDefault)
+                        defaultTargetUrl = target;
 
-                        bool isActive = filterContext.ActionDescriptor.ActionName == actionDescriptor.ActionName &&
-                                        filterContext.ActionDescriptor.ControllerDescriptor.ControllerType ==
-                                        descriptor.ControllerType;
+                    bool isActive = filterContext.ActionDescriptor.ActionName == actionDescriptor.ActionName &&
+                                    filterContext.ActionDescriptor.ControllerDescriptor.ControllerType ==
+                                    descriptor.ControllerType;
 
-                        if (isActive)
-                            actionText = actionAttr.Text;
+                    Navigation navigation = new Navigation(null,
+                                       actionAttr.Order,
+                                       actionAttr.Text,
+                                       target,
+                                       isActive,
+                                       Enumerable.Empty<Navigation>());
 
-                        Navigation navigation = new Navigation(null,
-                                                               actionAttr.Order,
-                                                               actionAttr.Text,
-                                                               target,
-                                                               isActive,
-                                                               Enumerable.Empty<Navigation>());
+                    if (isActive)
+                        active = navigation;
 
-                        children.Add(navigation);
-                    }
 
-                    bool isAnyChildActive = children.Any(n => n.IsActive);
 
-                    // if there's only one child, ignore it
-                    if (children.Count == 1)
-                        children.Clear();
-
-                    menu.Add(new Navigation(controllerAttr.Group,
-                                            controllerAttr.Order,
-                                            controllerAttr.Text,
-                                            defaultTargetUrl,
-                                            isAnyChildActive,
-                                            children));
+                    children.Add(navigation);
                 }
 
-                var navigations = menu.OrderBy(n => n.Order).ToArray();
-                model.Navigation = navigations;
+                bool isAnyChildActive = children.Any(n => n.IsActive);
 
-                model.PageTitle = "LostDoc Administration " + metadata.Text;
+                // if there's only one child, ignore it
+                if (children.Count == 1)
+                    children.Clear();
 
-                if (!string.IsNullOrWhiteSpace(actionText))
-                    model.PageTitle += string.Format(" - {0}", actionText);
+                menu.Add(new Navigation(controllerAttr.Group,
+                                        controllerAttr.Order,
+                                        controllerAttr.Text,
+                                        defaultTargetUrl,
+                                        isAnyChildActive,
+                                        children));
             }
+
+            var navigations = menu.OrderBy(n => n.Order).ToArray();
+            return navigations;
         }
 
         public void OnActionExecuting(ActionExecutingContext filterContext)
