0d786289042feba07309975596ac962c5d036c3a
Patrik Husfloen <patrik.husfloen@lbi.com>
2013-06-13 10:19:10 +0200

    Reformat

--- LBi.LostDoc.Repository.Web.Host/Areas/Administration/Controllers/LibraryController.cs
+++ LBi.LostDoc.Repository.Web.Host/Areas/Administration/Controllers/LibraryController.cs
@@ -41,10 +41,91 @@ namespace LBi.LostDoc.Repository.Web.Host.Areas.Administration.Controllers
             this.Notifications = notificationManager;
         }
 
-        protected NotificationManager Notifications { get; set; }
-
         [Import]
         protected new ContentManager Content { get; set; }
+
+        protected NotificationManager Notifications { get; set; }
+
+        public ActionResult Build()
+        {
+            this.Content.QueueRebuild("Requested by " + this.User.Identity.Name);
+            this.Notifications.Add(Severity.Information, 
+                                   Lifetime.Page, 
+                                   Scope.User, 
+                                   this.User, 
+                                   "Rebuilding Content", 
+                                   "A new content rebuild request has been added to the processing queue.");
+
+            return this.Redirect(this.Url.Action("Index"));
+        }
+
+        public ActionResult Delete(string id)
+        {
+            string contentRoot = this.Content.GetContentRoot(id);
+            Directory.Delete(contentRoot, true);
+            this.Notifications.Add(Severity.Information, 
+                                   Lifetime.Page, 
+                                   Scope.User, 
+                                   this.User, 
+                                   "Library Deleted", 
+                                   string.Format("The library with id {0} has been deleted.", id));
+
+            return this.Redirect(this.Url.Action("Index"));
+        }
+
+        public ActionResult Details(string id)
+        {
+            string contentRoot = App.Instance.Content.GetContentRoot(id);
+
+            var files = Directory.GetFiles(Path.Combine(contentRoot, "Source"), "*.ldoc");
+            var ldocs = files.Select(f => new LostDocFileInfo(f));
+            var groups = ldocs.GroupBy(ld => ld.PrimaryAssembly.AssetId);
+
+            List<AssemblyModel> assemblies = new List<AssemblyModel>();
+
+            foreach (var group in groups)
+            {
+                assemblies.Add(new AssemblyModel
+                                   {
+                                       Name = group.First().PrimaryAssembly.Name, 
+                                       Versions = group.Select(ld =>
+                                                               new VersionModel
+                                                                   {
+                                                                       Filename = Path.GetFileName(ld.Path), 
+                                                                       Created = System.IO.File.GetCreationTime(ld.Path), 
+                                                                       Version = ld.PrimaryAssembly.AssetId.Version
+                                                                   }).ToArray()
+                                   });
+            }
+
+            string htmlRoot = Path.Combine(contentRoot, "Html");
+
+            return this.View(new LibraryDetailsModel
+                                 {
+                                     Input = new ContentRepositoryModel
+                                                 {
+                                                     IsReadOnly = true, 
+                                                     Assemblies = assemblies.ToArray()
+                                                 }, 
+                                     OutputDataUrl = this.Url.Action("LibraryHtmlFiles", new { id }), 
+                                     OutputDownloadUrl = this.Url.Action("DownloadHtmlFile", new { id }).TrimEnd('/') + "?path=", 
+                                     OutputViewUrl = this.Url.RouteUrl("Archive", new { id, path = string.Empty }).TrimEnd('/'), 
+                                     LogDataUrl = this.Url.Action("LibraryLogFiles", new { id }), 
+                                     LogDownloadUrl = this.Url.Action("LogFile", new { id }).TrimEnd('/') + "?d=", 
+                                     LogViewUrl = this.Url.Action("LogFile", new { id, path = string.Empty }).TrimEnd('/') + "?v=", 
+                                     Created = Directory.GetCreationTime(htmlRoot)
+                                 });
+        }
+
+        public ActionResult DownloadHtmlFile(string id, string path)
+        {
+            return this.DownloadFile(id, "Html", path);
+        }
+
+        public ActionResult DownloadSourceFile(string id, string path)
+        {
+            return this.DownloadFile(id, "Source", path);
+        }
 
         [AdminAction("index", IsDefault = true)]
         public ActionResult Index()
@@ -56,10 +137,39 @@ namespace LBi.LostDoc.Repository.Web.Host.Areas.Administration.Controllers
 
             return this.View(new LibraryModel
                                  {
-                                     SystemState = App.Instance.Content.CurrentState,
-                                     Libraries = libraries.ToArray(),
+                                     SystemState = App.Instance.Content.CurrentState, 
+                                     Libraries = libraries.ToArray(), 
                                      Current = App.Instance.Content.ContentFolder
                                  });
+        }
+
+        public ActionResult LibraryHtmlFiles(string id, string dir)
+        {
+            return this.DirectoryListing(id, "Html", dir);
+        }
+
+        public ActionResult LibraryLogFiles(string id, string dir)
+        {
+            return this.DirectoryListing(id, "Logs", dir);
+        }
+
+        public ActionResult LogFile(string id, string v, string d)
+        {
+            return this.DownloadFile(id, "Logs", v ?? d, v == null);
+        }
+
+        public ActionResult SetCurrent(string id)
+        {
+            this.Content.SetCurrentContentFolder(id);
+            this.Notifications.Add(Severity.Information, 
+                                   Lifetime.Page, 
+                                   Scope.User, 
+                                   this.User, 
+                                   "New Library Content", 
+                                   "The library content has changed", 
+                                   NotificationActions.Refresh);
+
+            return this.Redirect(this.Url.Action("Index"));
         }
 
         private static LibraryDescriptorModel CreateLibraryModel(string d, string root)
@@ -67,28 +177,28 @@ namespace LBi.LostDoc.Repository.Web.Host.Areas.Administration.Controllers
             string value = XDocument.Load(Path.Combine(d, "info.xml")).Element("content").Attribute("created").Value;
             return new LibraryDescriptorModel
                        {
-                           Id = d.Substring(root.Length),
-                           Created = XmlConvert.ToDateTime(value,
+                           Id = d.Substring(root.Length), 
+                           Created = XmlConvert.ToDateTime(value, 
                                                            XmlDateTimeSerializationMode.Local)
                        };
         }
 
         private ActionResult DirectoryListing(string id, string folder, string path)
         {
-            path = (path ?? "").TrimStart('/');
+            path = (path ?? string.Empty).TrimStart('/');
             string contentRoot = App.Instance.Content.GetContentRoot(id);
             string folderPath = Path.Combine(contentRoot, folder);
             string contentDir = Path.Combine(folderPath, path);
 
             DirectoryInfo di = new DirectoryInfo(contentDir);
 
-            return View("_DirectoryListing",
-                        new DirectoryListModel
-                        {
-                            Root = new DirectoryInfo(folderPath),
-                            Directories = di.GetDirectories(),
-                            Files = di.GetFiles()
-                        });
+            return this.View("_DirectoryListing", 
+                             new DirectoryListModel
+                                 {
+                                     Root = new DirectoryInfo(folderPath), 
+                                     Directories = di.GetDirectories(), 
+                                     Files = di.GetFiles()
+                                 });
         }
 
         private ActionResult DownloadFile(string id, string folder, string path, bool asDownload = true)
@@ -109,118 +219,5 @@ namespace LBi.LostDoc.Repository.Web.Host.Areas.Administration.Controllers
 
             throw new HttpException((int)HttpStatusCode.Forbidden, "Forbidden!");
         }
-
-        public ActionResult Details(string id)
-        {
-            string contentRoot = App.Instance.Content.GetContentRoot(id);
-
-            var files = Directory.GetFiles(Path.Combine(contentRoot, "Source"), "*.ldoc");
-            var ldocs = files.Select(f => new LostDocFileInfo(f));
-            var groups = ldocs.GroupBy(ld => ld.PrimaryAssembly.AssetId);
-
-            List<AssemblyModel> assemblies = new List<AssemblyModel>();
-
-            foreach (var group in groups)
-            {
-                assemblies.Add(new AssemblyModel
-                                   {
-                                       Name = group.First().PrimaryAssembly.Name,
-                                       Versions = group.Select(ld =>
-                                                               new VersionModel
-                                                                   {
-                                                                       Filename = Path.GetFileName(ld.Path),
-                                                                       Created = System.IO.File.GetCreationTime(ld.Path),
-                                                                       Version = ld.PrimaryAssembly.AssetId.Version
-                                                                   }).ToArray()
-                                   });
-            }
-
-            string htmlRoot = Path.Combine(contentRoot, "Html");
-
-            return this.View(new LibraryDetailsModel
-                                 {
-                                     Input = new ContentRepositoryModel
-                                                 {
-                                                     IsReadOnly = true,
-                                                     Assemblies = assemblies.ToArray()
-                                                 },
-                                     OutputDataUrl = this.Url.Action("LibraryHtmlFiles", new { id }),
-                                     OutputDownloadUrl =
-                                         this.Url.Action("DownloadHtmlFile", new { id }).TrimEnd('/') + "?path=",
-                                     OutputViewUrl = this.Url.RouteUrl("Archive", new { id, path = "" }).TrimEnd('/'),
-                                     LogDataUrl = this.Url.Action("LibraryLogFiles", new { id }),
-                                     LogDownloadUrl = this.Url.Action("LogFile", new { id }).TrimEnd('/') + "?d=",
-                                     LogViewUrl = this.Url.Action("LogFile", new { id, path = "" }).TrimEnd('/') + "?v=",
-                                     Created = Directory.GetCreationTime(htmlRoot)
-                                 });
-        }
-
-
-        public ActionResult LogFile(string id, string v, string d)
-        {
-            return this.DownloadFile(id, "Logs", v ?? d, v == null);
-        }
-
-        public ActionResult LibraryHtmlFiles(string id, string dir)
-        {
-            return DirectoryListing(id, "Html", dir);
-        }
-
-        public ActionResult LibraryLogFiles(string id, string dir)
-        {
-            return DirectoryListing(id, "Logs", dir);
-        }
-
-        public ActionResult DownloadHtmlFile(string id, string path)
-        {
-            return DownloadFile(id, "Html", path);
-        }
-
-        public ActionResult DownloadSourceFile(string id, string path)
-        {
-            return DownloadFile(id, "Source", path);
-        }
-
-        public ActionResult Delete(string id)
-        {
-            string contentRoot = this.Content.GetContentRoot(id);
-            Directory.Delete(contentRoot, true);
-            this.Notifications.Add(Severity.Information,
-                                   Lifetime.Page,
-                                   Scope.User,
-                                   this.User,
-                                   "Library Deleted",
-                                   string.Format("The library with id {0} has been deleted.", id));
-
-            return Redirect(Url.Action("Index"));
-        }
-
-        public ActionResult SetCurrent(string id)
-        {
-            this.Content.SetCurrentContentFolder(id);
-            this.Notifications.Add(Severity.Information,
-                                   Lifetime.Page,
-                                   Scope.User,
-                                   this.User,
-                                   "New Library Content",
-                                   "The library content has changed",
-                                   NotificationActions.Refresh);
-
-            return Redirect(Url.Action("Index"));
-        }
-
-        public ActionResult Build()
-        {
-            this.Content.QueueRebuild("Requested by " + this.User.Identity.Name);
-            this.Notifications.Add(Severity.Information,
-                                   Lifetime.Page,
-                                   Scope.User,
-                                   this.User,
-                                   "Rebuilding Content",
-                                   "A new content rebuild request has been added to the processing queue.");
-
-            return Redirect(Url.Action("Index"));
-        }
-
     }
 }
