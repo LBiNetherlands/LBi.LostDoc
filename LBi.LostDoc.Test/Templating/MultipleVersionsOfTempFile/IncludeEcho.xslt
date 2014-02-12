5d7fa8124b3d013a17f9e243ee13a20b2398c289
Patrik Husfloen <patrik.husfloen@lbi.com>
2014-02-12 17:00:38 +0100

    Add test for versioned output files.

---
+++ LBi.LostDoc.Test/Templating/MultipleVersionsOfTempFile/IncludeEcho.xslt
@@ -0,0 +1,14 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:msxsl="urn:schemas-microsoft-com:xslt"
+                exclude-result-prefixes="msxsl">
+
+  <xsl:output method="xml" indent="yes"/>
+
+  <xsl:template match="/">
+    <root>
+      <xsl:value-of select="document('temp://echo.xml')/data"/>
+    </root>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ LBi.LostDoc.Test/Templating/TemplateTests.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using LBi.LostDoc.Templating;
+using LBi.LostDoc.Templating.IO;
+using Xunit;
+
+namespace LBi.LostDoc.Test.Templating
+{
+    public class TemplateTests
+    {
+        [Fact]
+        public void MultipleVersionsOfTempFile()
+        {
+            IFileProvider templateProvider = new ResourceFileProvider("LBi.LostDoc.Test.Templating", Assembly.GetExecutingAssembly());
+
+            TemplateInfo tInfo = new TemplateInfo(templateProvider,
+                                                  "MultipleVersionsOfTempFile/template.xml",
+                                                  "MultipleVersionsOfTempFile",
+                                                  new TemplateParameterInfo[0],
+                                                  null);
+
+            Template template = tInfo.Load();
+
+            TemporaryFileProvider tempFiles = new TemporaryFileProvider();
+            TemplateOutput output = template.Generate(new XDocument(),
+                                                      new TemplateSettings
+                                                      {
+                                                          OutputFileProvider = tempFiles,
+                                                      });
+
+            Assert.Equal(3, tempFiles.GetFiles("").Count());
+            Assert.Contains("Test1.xml", tempFiles.GetFiles(""));
+            Assert.Contains("Test2.xml", tempFiles.GetFiles(""));
+            Assert.Contains("Test3.xml", tempFiles.GetFiles(""));
+
+            using (var file = tempFiles.OpenFile("Test1.xml", FileMode.Open))
+            {
+                Assert.Equal("one", XDocument.Load(file).Root.Value);
+            }
+
+            using (var file = tempFiles.OpenFile("Test2.xml", FileMode.Open))
+            {
+                Assert.Equal("two", XDocument.Load(file).Root.Value);
+            }
+
+            using (var file = tempFiles.OpenFile("Test3.xml", FileMode.Open))
+            {
+                Assert.Equal("three", XDocument.Load(file).Root.Value);
+            }
+
+            tempFiles.Delete();
+        }
+    }
+}
+
